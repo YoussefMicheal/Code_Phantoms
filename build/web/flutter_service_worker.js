d781a439b36f57fbb612f5ff303c96a8d5546af9
Youssef Micheal <yousefmicheal0@gmail.com>
2025-10-30 21:16:59 +0300

    build web for github pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "dab5ad28d187d0a00282263c6349cb01
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3795c13b55f96d7262aa995f647239f8",
+"flutter_bootstrap.js": "1a9e6c05a937ed7e9ae8aff6d5a13b6f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
